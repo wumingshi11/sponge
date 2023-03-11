c42fe985996e06f4f8c9462e1d2c9aaa6bce6876
ZhaoXiaoming <18755187477@163.com>
2023-03-11 16:36:48 +0800

    完成lab1

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,4 +1,5 @@
 #include "stream_reassembler.hh"
+#include <iostream>
 
 // Dummy implementation of a stream reassembler.
 
@@ -12,15 +13,34 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) ,streams(){}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+
+    for (size_t i = 0; i < data.size(); i++) {
+      if (_output.bytes_written() > index + i) continue;
+      if (i + index - _output.bytes_read() > _capacity) break;
+      streams.emplace(data[i], index + i);
+    }
+
+    string s;
+    size_t s_index = _output.bytes_written();
+    while (!empty() && streams.begin()->index == s_index) {
+      s.push_back(streams.begin()->c);
+      streams.erase(streams.begin());
+      s_index++;
+    }
+    _output.write(s);     
+
+    if (eof) _eof_index = index + data.size();
+    if (_output.bytes_written() == _eof_index) {
+      _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return streams.size(); }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return streams.empty(); }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,15 +5,32 @@
 
 #include <cstdint>
 #include <string>
+#include <set>
+
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-
+    class substream {
+      public:
+        size_t index;
+        char c;
+        substream(const char cc, size_t i) : index(i),c(cc)  {};
+        bool operator< (const substream& rhs) const{
+          return index < rhs.index;
+        }
+        bool operator== (const substream& rhs) const{
+          return index == rhs.index;
+        }
+    };
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    size_t _unassembled_size{0}; //!< the unassembler number of bytes
+    size_t _next_index{0};
+    size_t _eof_index{UINT64_MAX};
+    std::set<substream> streams; //! save the unassembled substring
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
